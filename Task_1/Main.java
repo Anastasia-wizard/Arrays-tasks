a4182996bbc7fed344926410b6e8d9588b4c1932
Anastasia-wizard <73994530+Anastasia-wizard@users.noreply.github.com>
2020-12-09 12:37:55 +0300

    Create Main.java

---
+++ Task_1/Main.java
@@ -0,0 +1,10 @@
+// 1.	Вывести массив
+public class Array {
+    public static void main(String[] args) {
+        int[] myArray = new int[] {2,4,3,6,17,25,49,100500,457,321,678};
+        for (int i = 0; i < myArray.length; i++) {
+            System.out.println(myArray[i]);
+
+        }
+    }
+}
