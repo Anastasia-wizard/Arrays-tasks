1c7b55a11ba86ec2a441603bb32de987a9b522da
Anastasia-wizard <73994530+Anastasia-wizard@users.noreply.github.com>
2020-12-10 15:46:33 +0300

    Create Main.java

---
+++ Task_6/Main.java
@@ -0,0 +1,13 @@
+//6.	Вывести индексы четных элементов массива.
+public class Array {
+    public static void main(String[] args) {
+        int[] myArray = new int[]{2, 4, 3, 6, 17, 25, 49, 100500, 457, 321, 678};
+        for (int i = 0; i < myArray.length; i++) {
+            if (i % 2 == 0 ){
+                System.out.println(i);
+
+            }
+        }
+
+    }
+}
