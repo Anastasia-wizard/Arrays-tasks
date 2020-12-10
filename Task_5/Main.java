8d6d1d69b00ae345b60971d9b18545d5601b4b3f
Anastasia-wizard <73994530+Anastasia-wizard@users.noreply.github.com>
2020-12-10 15:54:11 +0300

    Create Main.java

---
+++ Task_5/Main.java
@@ -0,0 +1,12 @@
+//5.	Вывести четные элементы массива.
+public class Array {
+    public static void main(String[] args) {
+        int[] myArray = new int[]{2, 4, 3, 6, 17, 25, 49, 100500, 457, 321, 678};
+        for (int i = 0; i < myArray.length; i++) {
+            if (myArray[i] % 2 == 0){
+                System.out.println(myArray[i]);
+
+            }
+        }
+    }
+}
