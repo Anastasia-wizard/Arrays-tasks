83abc5a98ab18b68bce00cb67d57e680467cae34
Anastasia-wizard <73994530+Anastasia-wizard@users.noreply.github.com>
2020-12-11 13:27:34 +0300

    Update Main.java

--- Task_1/Main.java
+++ Task_1/Main.java
@@ -1,10 +1,10 @@
-// 1.	Вывести массив
+//1.	Вывести массив
 public class Array {
     public static void main(String[] args) {
-        int[] myArray = new int[] {2,4,3,6,17,25,49,100500,457,321,678};
+        int[] myArray = new int[50];
         for (int i = 0; i < myArray.length; i++) {
-            System.out.println(myArray[i]);
-
+            myArray[i] = (int) Math.round((Math.random() * 20) - 10);
+            System.out.print(" " +myArray[i]);
         }
     }
 }
