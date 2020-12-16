dfc98a627bdc714ab5066645369ebe736ad3c769
Anastasia-wizard <73994530+Anastasia-wizard@users.noreply.github.com>
2020-12-16 23:05:55 +0300

    Update Main.java

--- Task_15/Main.java
+++ Task_15/Main.java
@@ -1,6 +1,3 @@
-import java.util.Arrays;
-import java.util.Scanner;
-
 // 15.	Вывести на экран среднее всех чисел в массиве.
 public class Array {
     public static void main(String[] args) {
@@ -8,12 +5,12 @@ public class Array {
         for (int i = 0; i < myArray.length; i++) {
             myArray[i] = (int) Math.round((Math.random() * 20) - 10);
         }
-int sum =0;
+        double sum =0;
         for (int j = 0; j < myArray.length; j++) {
             sum = sum + myArray[j];
 
         }
-        sum= -sum;
+        
 
         System.out.println(sum/ myArray.length);
     }
